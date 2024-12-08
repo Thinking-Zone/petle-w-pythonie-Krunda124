43c20f07597c5e5cfbe817c081274c8d47c4e438
Krunda124 <amelia437.prochowicz@gmail.com>
2024-12-08 16:23:40 +0100

    Update zadanie8.py

---
+++ zadanie8.py
@@ -0,0 +1,14 @@
+pada = False
+licznik_nie = 0
+while not pada:
+    print("Nie pada! yaaay")
+    odpowiedz = input("Czy pada? (tak/nie) ")
+    if odpowiedz = "nie":
+      licznik_nie = licznik_nie + 1
+    elif odpowiedz = "nie wiem":
+      print("To wyjdź na zewnątrz i się dowiedz.")
+    elif odpowiedz = "tak":
+      print("odpowiedziałes nie tyle razy: " + licznik_nie) 
+      pada = True
+    else:
+      print("nie rozumiem twojej odpowiedzi")
